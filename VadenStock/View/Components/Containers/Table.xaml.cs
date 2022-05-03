30849c95074071047c397e8a1826e865ea99a6f6
Felipe Sousa <sousa.felipe@oulook.com>
2022-05-03 07:34:07 -0300

    Table

--- VadenStock/View/Components/Containers/Table.xaml.cs
+++ VadenStock/View/Components/Containers/Table.xaml.cs
@@ -10,23 +10,65 @@ namespace VadenStock.View.Components.Containers
 {
     public partial class Table : Border
     {
+        public List<Header> Headers { get; private set; }
+        public List<Row> Rows { get; private set; }
+
+
+
         public Table()
         {
             InitializeComponent();
+
+            Headers = new();
+            Rows = new();
+        }
+
+
+
+        public void Draw()
+        {
+            foreach (Header header in Headers)
+            {
+                //_GridContainer.Children.Add()
+            }
         }
 
 
 
         public class Header
         {
-            
+            public string Content { get; private set; }
+
+
+            private Header(string content)
+            {
+                Content = content;
+            }
+
+
+            public static Header Data(string data)
+            {
+                return new Header(data);
+            }
         }
 
 
 
         public class Row
         {
+            public string Content { get; private set; }
 
+
+            private Row(string content)
+            {
+                Content = content;
+            }
+
+
+            public static Row Data(string data)
+            {
+                return new Row(data);
+            }
         }
     }
 }
